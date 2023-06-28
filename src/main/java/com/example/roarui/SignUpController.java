e8b100a7dc8c18969bb036dcb304853a5f5f3341
Amirhossein <Amirmemool12@gmail.com>
2023-06-28 18:36:43 -0400

    :wastebasket: style: clean up the SignUpController class

--- src/main/java/com/example/roarui/SignUpController.java
+++ src/main/java/com/example/roarui/SignUpController.java
@@ -1,12 +1,10 @@
 package com.example.roarui;
 
-import com.example.roarui.Component.Util.Util;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
