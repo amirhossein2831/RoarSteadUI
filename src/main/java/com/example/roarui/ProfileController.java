75e4c474f9428c582998698fc46fc35577a6ac3d
Amirhossein <Amirmemool12@gmail.com>
2023-06-29 08:29:21 -0400

    :hammer: chore:set action for profile and header image

--- src/main/java/com/example/roarui/ProfileController.java
+++ src/main/java/com/example/roarui/ProfileController.java
@@ -1,5 +1,6 @@
 package com.example.roarui;
 
+import com.example.roarui.Component.Util.Util;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,14 +12,21 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static com.example.roarui.Component.Util.Util.openLink;
+
 public class ProfileController implements Initializable {
     @FXML
     private ImageView profImageView;
+
+    @FXML
+    ImageView headerImageView;
     Circle circle;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         createCircle();
+        headerImageView.setOnMouseClicked(event-> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/istockphoto-178488809-612x612.jpg"));
+        profImageView.setOnMouseClicked(event -> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/l4.png"));
     }
 
     private void createCircle() {
