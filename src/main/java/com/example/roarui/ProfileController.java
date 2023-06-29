021f159de3ecdcf51700f91579ae7b1899085a7d
Amirhossein <Amirmemool12@gmail.com>
2023-06-29 11:22:52 -0400

    :lipstick: chore: change the size of profile cirle

--- src/main/java/com/example/roarui/ProfileController.java
+++ src/main/java/com/example/roarui/ProfileController.java
@@ -1,20 +1,14 @@
 package com.example.roarui;
 
-import com.example.roarui.Component.Util.Util;
-import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static com.example.roarui.Component.Util.Util.*;
+import static com.example.roarui.Component.Util.Util.goTo;
+import static com.example.roarui.Component.Util.Util.openLink;
 
 public class ProfileController extends AppController {
     @FXML
@@ -23,19 +17,18 @@ public class ProfileController extends AppController {
     @FXML
     private ImageView headerImageView;
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         logOutPageAction();
         createProfileCircle();
-        homeBut.setOnAction(event -> goTo(event,"app","HOME"));
+        homeBut.setOnAction(event -> goTo(event, "app", "HOME"));
         //TODO change the below link with the user link
-        headerImageView.setOnMouseClicked(event-> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/istockphoto-178488809-612x612.jpg"));
+        headerImageView.setOnMouseClicked(event -> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/istockphoto-178488809-612x612.jpg"));
         profImageView.setOnMouseClicked(event -> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/l4.png"));
     }
 
     private void createProfileCircle() {
-        Circle circle = new Circle(75, 75, 75);
+        Circle circle = new Circle(67, 67, 67);
         profImageView.setClip(circle);
     }
 }
