f5665405a95b65958df3fa06a368a0f50796fcd7
Amirhossein <Amirmemool12@gmail.com>
2023-06-29 09:56:48 -0400

    :fire: chore: implement the button to the ProfileController that inheritanc from its parent AppController

--- src/main/java/com/example/roarui/ProfileController.java
+++ src/main/java/com/example/roarui/ProfileController.java
@@ -5,6 +5,8 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -12,25 +14,22 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static com.example.roarui.Component.Util.Util.CIRCLE_PROFILE;
 import static com.example.roarui.Component.Util.Util.openLink;
 
-public class ProfileController implements Initializable {
+public class ProfileController extends AppController {
     @FXML
     private ImageView profImageView;
 
     @FXML
-    ImageView headerImageView;
-    Circle circle;
+    private ImageView headerImageView;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        createCircle();
+        logOutPageAction();
+        profImageView.setClip(CIRCLE_PROFILE);
         headerImageView.setOnMouseClicked(event-> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/istockphoto-178488809-612x612.jpg"));
         profImageView.setOnMouseClicked(event -> openLink("/home/amirhossein/IdeaProjects/RoarUi/src/main/resources/com/example/roarui/image/l4.png"));
-    }
-
-    private void createCircle() {
-        circle = new Circle(75, 75, 75);
-        profImageView.setClip(circle);
     }
 }
