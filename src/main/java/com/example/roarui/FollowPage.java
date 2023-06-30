dbe4a9cec8381928665f61db242f889e88e6fdfa
Amirhossein <Amirmemool12@gmail.com>
2023-06-30 09:12:11 -0400

    :hammer: feat: create the followController

---
+++ src/main/java/com/example/roarui/FollowPage.java
@@ -0,0 +1,13 @@
+package com.example.roarui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class FollowPage extends AppController{
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        logOutPageAction();
+    }
+}
