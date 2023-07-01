7ffdd6539ce11aded77d5cd3bb0bab5d863f1a19
Amirhossein <Amirmemool12@gmail.com>
2023-07-01 13:39:40 -0400

    :hammer: chore:set circle shape to imageView

--- src/main/java/com/example/roarui/RoarController.java
+++ src/main/java/com/example/roarui/RoarController.java
@@ -3,6 +3,8 @@ package com.example.roarui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -12,10 +14,14 @@ public class RoarController implements Initializable {
     @FXML
     private Button closeBut;
 
+    @FXML
+    private ImageView profView;
+
     private boolean backScreenDisabled;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        createProfileCircle();
         closeBut.setOnAction(event -> closeFrontScreen());
     }
     public void setBackScreenDisabled(boolean disabled) {
@@ -30,5 +36,9 @@ public class RoarController implements Initializable {
             backStage.getScene().getRoot().setEffect(null); // Remove blur effect from back stage
             backStage.getScene().getRoot().setDisable(false);
         }
+    }
+    protected void createProfileCircle() {
+        Circle circle = new Circle(25, 25, 25);
+        profView.setClip(circle);
     }
 }
